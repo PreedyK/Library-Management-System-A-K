b0154d0399b8a4f9e371fe33ff690b07d4d2bde4
Kiryn Preedy <kiryn.preedy@gmail.com>
2025-10-17 09:24:49 -0500

    Add files via upload

---
+++ LMS_members.py
@@ -0,0 +1,15 @@
+def init_(self):
+    self.member_id = ""
+    self.member_name = ""
+    self.libCardnumber = ""
+    self.password = ""
+    self.booksborrowed = []
+
+
+def add_member(self):
+    self.member_id = input("Enter Member ID:")
+    self.member_name = input("Enter Member Name:")
+    self.libCardnumber = input("Enter Library Card Number:")
+    self.password = input("Create Password:")
+
+def remove_member(self):
