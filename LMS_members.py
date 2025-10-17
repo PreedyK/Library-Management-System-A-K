7a222d70fa074559a990d62e93ae9f6a5047d55a
Kiryn Preedy <kiryn.preedy@gmail.com>
2025-10-17 09:43:24 -0500

    LMS_members.py

--- LMS_members.py
+++ LMS_members.py
@@ -1,15 +1,17 @@
-def init_(self):
-    self.member_id = ""
-    self.member_name = ""
-    self.libCardnumber = ""
-    self.password = ""
-    self.booksborrowed = []
+class Member: 
+    def init_(self):
+        self.member_id = ""
+        self.member_name = ""
+        self.libCardnumber = ""
+        self.password = ""
+        self.booksborrowed = []
 
 
-def add_member(self):
-    self.member_id = input("Enter Member ID:")
-    self.member_name = input("Enter Member Name:")
-    self.libCardnumber = input("Enter Library Card Number:")
-    self.password = input("Create Password:")
+    def add_member(self):
+        self.member_id = input("Enter Member ID:")
+        self.member_name = input("Enter Member Name:")
+        self.libCardnumber = input("Enter Library Card Number:")
+        self.password = input("Create Password:")
 
-def remove_member(self):
+    def remove_member(self):
+
